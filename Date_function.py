cd8c5c1c483c554a85da52ee0f9d3311a15b976e
EmilHuan <77182617+EmilHuan@users.noreply.github.com>
2021-02-01 16:47:59 +0800

    Develop "leap year automation" (#1)
    * add leap year.py and modify
    
    * leap year automation

--- Date_function.py
+++ Date_function.py
@@ -11,89 +11,79 @@ year = datetime.date.today().year
 # month：月份 (1~12)
 # week_number：當月一號從星期幾開始 (用數字 1~7 代替中文字)
 # Feb_leap_or_not：二月是否遇到閏年 (預設 = 28 天，非閏年。閏年需設定為 29)
-def print_date(month, week_number, Feb_leap_or_not = 28):
+def print_date(month, week_number):
     # 生成一個數字 1~12 的 tuple (if 條件檢查用)
     month_tuple = tuple(i for i in range(1, 13))
     # 生成一個數字 1~7 的 tuple (if 條件檢查用)
     week_number_tuple = tuple(i for i in range(1, 8))
 
     # 如果 month & week_number 的引數都有在 tuple 裡 (月份符合 1~12，星期符合 1~7)，則執行印出程式
-    if week_number in week_number_tuple and month in month_tuple:
+    if (week_number in week_number_tuple) and (month in month_tuple):
         # 先形成一個中文字一到日、重複 7 次的 tuple，用於印出中文的星期
         week = ("一", "二", "三", "四", "五", "六", "日")*7
 
-        # 如 Feb_leap_or_not 為預設的 28，則執行平年規則
-        if Feb_leap_or_not == 28:
-            # 如 month == 1, 3, 5, 7, 8 月，印出時月份前加 0，共要印出 31 天份的日期
-            if month in (1, 3, 5, 7, 8):
-                # 1~ 9 號印出時，日期前面加 0
+        # 如 month == 1, 3, 5, 7, 8 月，印出時月份前加 0，共要印出 31 天份的日期
+        if month in (1, 3, 5, 7, 8):
+            # 1~ 9 號印出時，日期前面加 0
+            for i in range(9):
+                print("{}/0{}/0{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
+                print()
+            # 9~31 號印出時，日期前面不加 0
+            for i in range(9, 31):
+                print("{}/0{}/{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
+                print()
+
+        # 如 month == 10, 12 月，印出時月份前不加 0，共要印出 31 天份的日期
+        elif month in (10, 12):
+            # 1~ 9 號印出時，日期前面加 0
+            for i in range(9):
+                print("{}/{}/0{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
+                print()
+            # 9~31 號印出時，日期前面不加 0
+            for i in range(9, 31):
+                print("{}/{}/{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
+                print()
+
+        # 如 month == 4, 6, 9 月，印出時月份前加 0，共要印出 30 天份的日期
+        elif month in (4, 6, 9):
+            # 1~ 9 號印出時，日期前面加 0
+            for i in range(9):
+                print("{}/0{}/0{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
+                print()
+            # 9~31 號印出時，日期前面不加 0
+            for i in range(9, 30):
+                print("{}/0{}/{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
+                print()
+
+        # 如 month == 11 月，印出時月份前不加 0，共要印出 30 天份的日期
+        elif month == 11:
+            # 1~ 9 號印出時，日期前面加 0
+            for i in range(9):
+                print("{}/{}/0{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
+                print()
+            # 9~31 號印出時，日期前面不加 0
+            for i in range(9, 30):
+                print("{}/{}/{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
+                print()
+
+        # 如 month == 2 月，則先判斷年份為閏年 or 平年，再印出相應的日期數 (閏年 29 天、平年 28 天)
+        if month == 2:
+            # 閏年判斷式 (如結果為 True，則 year 為閏年)，通過則印出 29 天
+            if (year%400 == 0) or ((year%4 == 0) and (year%100 != 0)):
                 for i in range(9):
                     print("{}/0{}/0{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
                     print()
-                # 9~31 號印出時，日期前面不加 0
-                for i in range(9, 31):
-                    print("{}/0{}/{} ({})：".format(year, month, i+1, week[i + (week_number - 1)]))
+                for i in range(9, 29):
+                    print("{}/0{}/{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
                     print()
-
-            # 如 month == 10, 12 月，印出時月份前不加 0，共要印出 31 天份的日期
-            elif month in (10, 12):
-                # 1~ 9 號印出時，日期前面加 0
-                for i in range(9):
-                    print("{}/{}/0{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
-                    print()
-                # 9~31 號印出時，日期前面不加 0
-                for i in range(9, 31):
-                    print("{}/{}/{} ({})：".format(year, month, i+1, week[i + (week_number - 1)]))
-                    print()
-
-            # 如 month == 4, 6, 9 月，印出時月份前加 0，共要印出 30 天份的日期
-            elif month in (4, 6, 9):
-                # 1~ 9 號印出時，日期前面加 0
-                for i in range(9):
-                    print("{}/0{}/0{} ({})：".format(year, month, i+1, week[i + (week_number - 1)]))
-                    print()
-                # 9~31 號印出時，日期前面不加 0
-                for i in range(9, 30):
-                    print("{}/0{}/{} ({})：".format(year, month, i+1, week[i + (week_number - 1)]))
-                    print()
-
-            # 如 month == 11 月，印出時月份前不加 0，共要印出 30 天份的日期
-            elif month == 11:
-                # 1~ 9 號印出時，日期前面加 0
-                for i in range(9):
-                    print("{}/{}/0{} ({})：".format(year, month, i+1, week[i + (week_number - 1)]))
-                    print()
-                # 9~31 號印出時，日期前面不加 0
-                for i in range(9, 30):
-                    print("{}/{}/{} ({})：".format(year, month, i+1, week[i + (week_number - 1)]))
-                    print()
-
-            # 如 month == 2 月，則使用 Feb_leap_or_not 引數作為印出日期數 (這邊預設 28 天，平年)
+            # 閏年判斷式回傳 False (year 為平年)，則印出 28 天
             else:
                 for i in range(9):
                     print("{}/0{}/0{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
                     print()
-                for i in range(9, Feb_leap_or_not):
+                for i in range(9, 28):
                     print("{}/0{}/{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
                     print()
-
-        # 如 Feb_leap_or_not == 29 天，則使用閏年規則 (2 月有 29 天) 印製日期
-        elif Feb_leap_or_not == 29:
-            # 如 month == 2，印出 2 月份 29 天日期
-            if month == 2:
-                for i in range(9):
-                    print("{}/0{}/0{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
-                    print()
-                for i in range(9, Feb_leap_or_not):
-                    print("{}/0{}/{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
-                    print()
-            # 如 month != 2 月，則印出錯誤訊息
-            else:
-                print("錯誤！閏月月份需為二月")
-
-        # 如 Feb_leap_or_not 為 28, 29 以外數字，則印出錯誤訊息
-        else:
-            print("錯誤！閏月天數只能是 28 或 29 天")
 
     # 如只有 week_number 引數在 week_number_tuple 裡，則印出 "月份輸入超出範圍" 訊息
     elif week_number in week_number_tuple:
@@ -110,10 +100,15 @@ def print_date(month, week_number, Feb_leap_or_not = 28):
 if __name__ == "__main__":
     # 測試預設情況
     print_date(6, 2)
-    # 測試非閏月 Feb_leap_or_not 引數輸入 29
-    print_date(3, 2, 29)
-    # 測試閏月 Feb_leap_or_not 引數輸入非 28, 29 數字
-    print_date(2, 2, 30)
-
-
-
+    # 分隔線
+    print("---------------------")
+    # 測試 2 月自動判斷閏年印出情況
+    print_date(2, 2)
+    # 分隔線
+    print("---------------------")
+    # 測試輸入月份超出範圍情況
+    print_date(13, 1)
+    # 測試輸入星期超出範圍情況
+    print_date(3, 8)
+    # 測試月份及星期皆超出範圍的情況
+    print_date(13, 9)
--- main.py
+++ main.py
@@ -9,13 +9,6 @@ while True:
         month = eval(input("請輸入月份 (1~12)："))
         week_number = eval(input("請輸入當月一號星期數字 (1~7):"))
 
-        # 如月分 == 2 月，請使用者輸入潤月天數 (28 or 29)
-        if month == 2:
-            Feb_leap_or_not = eval(input("輸入二月天數 (28 或 29)："))
-        # 如月份 != 2 月，天數參數一律定為預設的 28 天
-        else:
-            Feb_leap_or_not = 28
-
     # SyntaxError 例外，輸入值出現特殊字元時回傳錯誤訊息
     except SyntaxError:
         print("錯誤！輸入值不能有特殊字元，請重新輸入")
@@ -27,7 +20,7 @@ while True:
 
     # 如例外檢查通過，執行 Date_function.py 檔案裡的函數 print_date
     else:
-        Date_function.print_date(month, week_number, Feb_leap_or_not)
+        Date_function.print_date(month, week_number)
         # 函數執行完畢後顯示感謝使用者的話
         print("感謝您的使用！")
         # 終止 while True
