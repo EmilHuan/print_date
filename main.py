55c1b5b3ebd16889902963e5afd355cfbf045ffe
EmilHuan <s93yoyo695072@gmail.com>
2021-07-06 23:39:35 +0800

    星期數字自動化

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ while True:
     try:
         # while True 檢查的主程式，請使用者輸入月份、星期幾、日期間要空幾行
         month = eval(input("請輸入月份 (1~12)："))
-        week_number = eval(input("請輸入當月一號星期數字 (1~7):"))
         # blank_number_chr 先不轉換成數字，且去掉左右兩側空格
         blank_number_chr = (input("請輸入每個日期間的空行數 (不輸入則使用預設值空一行)：")).strip()
 
@@ -29,11 +28,8 @@ while True:
 
     # 如例外檢查通過，執行 Date_function.py 檔案裡的函數 print_date
     else:
-        Date_function.print_date(month, week_number, blank_number)
+        Date_function.print_date(month, blank_number)
         # 函數執行完畢後顯示感謝使用者的話
         print("感謝您的使用！")
-        # 終止 while True
+        # 終止 while True 
         break
-
-
-
--- test_datetime.py
+++ test_datetime.py
@@ -1,9 +1,33 @@
 import datetime
 
+# 印出系統年份
 year = datetime.date.today().year
 print(year)
+print(type(year))
 
+# 印出系統完整時間 (年、月、日、幾點幾分幾秒)
 year2 = datetime.datetime.now()
 print(year2)
 
-print(type(year))
+# 印出今天是星期幾 (回傳數字 + 1 = 真的星期幾)
+print(datetime.datetime.today().weekday())
+# 印出今天是星期幾 (回傳數字 = 真的星期幾)
+print(datetime.datetime.today().isoweekday())
+
+# 功能不太確定
+print(datetime.timedelta(days=1))
+
+# 印出指定月份日曆
+import calendar
+cal = calendar.month(2015, 12)
+print(cal)
+
+# 回傳指定日期是星期幾
+import datetime
+day = datetime.datetime(2021, 7, 1)
+print(day)
+print(day.isoweekday())
+print("星期%d" % (day.weekday() + 1))
+
+
+
