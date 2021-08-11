16d1e683603f422f8d9df2b4514bd1a00f3b53e5
EmilHuan <77182617+EmilHuan@users.noreply.github.com>
2021-08-11 21:31:11 +0800

    Develop (#2)
    * add leap year.py and modify
    
    * leap year automation
    
    * 新增「自訂空行數」功能
    
    * Create Date_function.cpython-38.pyc
    
    * Revert "Create Date_function.cpython-38.pyc"
    
    This reverts commit e92388c65b56cd81d39d9587e880d4d77643100f.
    
    * 星期數字自動化
    
    * 新增「是否要再次列印」功能

--- Date_function.py
+++ Date_function.py
@@ -6,109 +6,114 @@ import datetime
 # 讀取系統時間中的西元年份 (用於日期的印出)
 year = datetime.date.today().year
 
+
+### 印出空格 function (可自訂要空幾格)
+def blank_rows(number):
+    number += 1
+    for i in range(1, number):
+        i += 1
+        print("")
+
+
 ### function "print_date"
 ## 參數說明
 # month：月份 (1~12)
 # week_number：當月一號從星期幾開始 (用數字 1~7 代替中文字)
 # Feb_leap_or_not：二月是否遇到閏年 (預設 = 28 天，非閏年。閏年需設定為 29)
-def print_date(month, week_number):
+# blank_number：作為 blank_rows() 的引數，自訂空格數用
+def print_date(month, blank_number = 1):
     # 生成一個數字 1~12 的 tuple (if 條件檢查用)
     month_tuple = tuple(i for i in range(1, 13))
-    # 生成一個數字 1~7 的 tuple (if 條件檢查用)
-    week_number_tuple = tuple(i for i in range(1, 8))
 
-    # 如果 month & week_number 的引數都有在 tuple 裡 (月份符合 1~12，星期符合 1~7)，則執行印出程式
-    if (week_number in week_number_tuple) and (month in month_tuple):
+    # 如果 month 的引數有在 month_tuple 裡 (月份符合 1~12)，則執行印出程式
+    if month in month_tuple:
         # 先形成一個中文字一到日、重複 7 次的 tuple，用於印出中文的星期
-        week = ("一", "二", "三", "四", "五", "六", "日")*7
+        week_ch = ("一", "二", "三", "四", "五", "六", "日") * 7
+
+        ## 用 datetime 模組回傳使用者輸入月份 (month) 的 1 號是星期幾
+        # 將系統年份、使用者輸入月份 (month) 的 1 號設為變數 day (e.g 2021-07-01 00:00:00)
+        day = datetime.datetime(year, month, 1)
+        # 回傳 day 這天是出星期幾 (數字) 給變數 week_number
+        week_number = day.isoweekday()
 
         # 如 month == 1, 3, 5, 7, 8 月，印出時月份前加 0，共要印出 31 天份的日期
         if month in (1, 3, 5, 7, 8):
             # 1~ 9 號印出時，日期前面加 0
             for i in range(9):
-                print("{}/0{}/0{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
-                print()
+                print("{}/0{}/0{} ({})：".format(year, month, i + 1, week_ch[i + (week_number - 1)]))
+                # 使用 blank_rows 函數設定每個印出的日期要空幾格
+                blank_rows(blank_number)
             # 9~31 號印出時，日期前面不加 0
             for i in range(9, 31):
-                print("{}/0{}/{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
-                print()
+                print("{}/0{}/{} ({})：".format(year, month, i + 1, week_ch[i + (week_number - 1)]))
+                blank_rows(blank_number)
 
         # 如 month == 10, 12 月，印出時月份前不加 0，共要印出 31 天份的日期
         elif month in (10, 12):
             # 1~ 9 號印出時，日期前面加 0
             for i in range(9):
-                print("{}/{}/0{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
-                print()
+                print("{}/{}/0{} ({})：".format(year, month, i + 1, week_ch[i + (week_number - 1)]))
+                blank_rows(blank_number)
             # 9~31 號印出時，日期前面不加 0
             for i in range(9, 31):
-                print("{}/{}/{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
-                print()
+                print("{}/{}/{} ({})：".format(year, month, i + 1, week_ch[i + (week_number - 1)]))
+                blank_rows(blank_number)
 
         # 如 month == 4, 6, 9 月，印出時月份前加 0，共要印出 30 天份的日期
         elif month in (4, 6, 9):
             # 1~ 9 號印出時，日期前面加 0
             for i in range(9):
-                print("{}/0{}/0{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
-                print()
+                print("{}/0{}/0{} ({})：".format(year, month, i + 1, week_ch[i + (week_number - 1)]))
+                blank_rows(blank_number)
             # 9~31 號印出時，日期前面不加 0
             for i in range(9, 30):
-                print("{}/0{}/{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
-                print()
+                print("{}/0{}/{} ({})：".format(year, month, i + 1, week_ch[i + (week_number - 1)]))
+                blank_rows(blank_number)
 
         # 如 month == 11 月，印出時月份前不加 0，共要印出 30 天份的日期
         elif month == 11:
             # 1~ 9 號印出時，日期前面加 0
             for i in range(9):
-                print("{}/{}/0{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
-                print()
+                print("{}/{}/0{} ({})：".format(year, month, i + 1, week_ch[i + (week_number - 1)]))
+                blank_rows(blank_number)
             # 9~31 號印出時，日期前面不加 0
             for i in range(9, 30):
-                print("{}/{}/{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
-                print()
+                print("{}/{}/{} ({})：".format(year, month, i + 1, week_ch[i + (week_number - 1)]))
+                blank_rows(blank_number)
 
         # 如 month == 2 月，則先判斷年份為閏年 or 平年，再印出相應的日期數 (閏年 29 天、平年 28 天)
         if month == 2:
             # 閏年判斷式 (如結果為 True，則 year 為閏年)，通過則印出 29 天
-            if (year%400 == 0) or ((year%4 == 0) and (year%100 != 0)):
+            if (year % 400 == 0) or ((year % 4 == 0) and (year % 100 != 0)):
                 for i in range(9):
-                    print("{}/0{}/0{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
-                    print()
+                    print("{}/0{}/0{} ({})：".format(year, month, i + 1, week_ch[i + (week_number - 1)]))
+                    blank_rows(blank_number)
                 for i in range(9, 29):
-                    print("{}/0{}/{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
-                    print()
+                    print("{}/0{}/{} ({})：".format(year, month, i + 1, week_ch[i + (week_number - 1)]))
+                    blank_rows(blank_number)
             # 閏年判斷式回傳 False (year 為平年)，則印出 28 天
             else:
                 for i in range(9):
-                    print("{}/0{}/0{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
-                    print()
+                    print("{}/0{}/0{} ({})：".format(year, month, i + 1, week_ch[i + (week_number - 1)]))
+                    blank_rows(blank_number)
                 for i in range(9, 28):
-                    print("{}/0{}/{} ({})：".format(year, month, i + 1, week[i + (week_number - 1)]))
-                    print()
+                    print("{}/0{}/{} ({})：".format(year, month, i + 1, week_ch[i + (week_number - 1)]))
+                    blank_rows(blank_number)
 
-    # 如只有 week_number 引數在 week_number_tuple 裡，則印出 "月份輸入超出範圍" 訊息
-    elif week_number in week_number_tuple:
-        print("錯誤！月份範圍限於 1~12")
-    # 如只有 month 引數在 month_tuple 裡，則印出 "星期數字輸入超出範圍" 訊息
-    elif month in month_tuple:
-        print("錯誤！星期數字範圍限於 1~7")
-    # 如 month & week_number 引數都不在各自 tuple 範圍，則印出 "月份及星期數字皆超出範圍" 訊息
+    # 如 month 引數不在 month_tuple 範圍 (輸入月份超出 1~12)，則印出 "月份輸入超出範圍" 訊息
     else:
-        print("錯誤！月份及星期數字皆超出範圍")
+        print("錯誤！月份範圍限於 1~12")
 
 
 # Date_function.py 測試程式
 if __name__ == "__main__":
-    # 測試預設情況
-    print_date(6, 2)
+    # 測試預設情況 (空格參數 = 0，日期間不空格)
+    print_date(1 , 0)
     # 分隔線
     print("---------------------")
-    # 測試 2 月自動判斷閏年印出情況
-    print_date(2, 2)
+    # 測試 2 月自動判斷閏年印出情況 (空格參數使用預設值，省略)
+    print_date(2)
     # 分隔線
     print("---------------------")
-    # 測試輸入月份超出範圍情況
-    print_date(13, 1)
-    # 測試輸入星期超出範圍情況
-    print_date(3, 8)
-    # 測試月份及星期皆超出範圍的情況
-    print_date(13, 9)
+    # 測試輸入月份超出範圍情況 (空格參數使用預設值，省略)
+    print_date(13)
--- main.py
+++ main.py
@@ -5,9 +5,17 @@ import Date_function
 while True:
     # 例外處理
     try:
-        # while True 檢查的主程式，請使用者輸入月份、星期幾
+        # while True 檢查的主程式，請使用者輸入月份、星期幾、日期間要空幾行
         month = eval(input("請輸入月份 (1~12)："))
-        week_number = eval(input("請輸入當月一號星期數字 (1~7):"))
+        # blank_number_chr 先不轉換成數字，且去掉左右兩側空格
+        blank_number_chr = (input("請輸入每個日期間的空行數 (不輸入則使用預設值空一行)：")).strip()
+
+        # 如輸入 blank_number_chr 時直接按 Enter，則 blank_number == 1 (預設空一行)
+        if blank_number_chr == "":
+            blank_number = 1
+        # 如有輸入，則轉換為數字並當作 blank_number 的變數值 (自訂空幾行)
+        else:
+            blank_number = eval(blank_number_chr)
 
     # SyntaxError 例外，輸入值出現特殊字元時回傳錯誤訊息
     except SyntaxError:
@@ -20,8 +28,18 @@ while True:
 
     # 如例外檢查通過，執行 Date_function.py 檔案裡的函數 print_date
     else:
-        Date_function.print_date(month, week_number)
-        # 函數執行完畢後顯示感謝使用者的話
-        print("感謝您的使用！")
-        # 終止 while True
-        break
+        Date_function.print_date(month, blank_number)
+        print()
+        # 詢問使用者是否要繼續列印其它月份
+        next_print = input("要再列印其它月份嗎？(繼續列印請輸入 y，結束列印請按任意鍵)：")
+        if next_print.lower() == "y":
+            pass
+        else:
+            # 函數執行完畢後顯示感謝使用者的話
+            print("感謝您的使用！")
+            # 終止 while True (結束列印)
+            break
+
+            
+  
+  
